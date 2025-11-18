0509140bc5da436d522205f4f074297fbf099fb3
Samii Shabuse <sshabuse@gmail.com>
2025-11-18 17:01:23 -0500

    feature(search_worker): defined SEARCH_WORKER_H

--- src/search_worker.h
+++ src/search_worker.h
@@ -5,3 +5,24 @@
 #include <filesystem>
 #include <thread>
 #include "channel.h"
+
+struct Match {
+    std::thread::id thread_id;
+    std::filesystem::path file_path;
+    int line_number;
+    std::string line_content;
+};
+
+// Worker thread entry point
+void workerThreadFunc(
+    channel<std::filesystem::path>* fileChan,
+    channel<Match>* resultChan,
+    const std::string& target
+)
+
+// Printer thread entry point
+void printerThreadFunc(
+    channel<Match>* resultChan
+);
+
+#endif // SEARCH_WORKER_H
